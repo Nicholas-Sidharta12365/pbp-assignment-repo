762afc40fc835baaaacfc703a5c4dc31320f2eb7
Nicholas Sidharta <toxicancm@gmail.com>
2022-10-08 23:19:45 +0700

    AJAX :D

--- todolist/urls.py
+++ todolist/urls.py
@@ -10,5 +10,7 @@ urlpatterns= [
    path('login/', login_user, name="login_user"),
    path('register/', register, name="register"),
    path('logout/', logout_user, name="logout"),
-   path('create-task/', create_task, name="create_task")
+   path('create-task/', create_task, name="create_task"),
+   path('json/', show_json, name="show_json"),
+   path('add/', create_ajax, name='create_ajax'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,4 @@
+from multiprocessing.connection import wait
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
@@ -5,8 +6,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-import datetime
-from django.http import HttpResponseRedirect
+from django.core import serializers
+from django.http import HttpResponse
 from todolist.models import Task
 
 # Create your views here.
@@ -48,6 +49,7 @@ def todolist(request):
         'taskData' : data,
         'display_create': 'Create New Task',
         'display_logout': 'Logout',
+        'id' : request.user.id,
     }
 
     return render(request, "todolist.html", context)
@@ -56,6 +58,7 @@ def logout_user(request):
     logout(request)
     return redirect('todolist:login_user')
 
+@login_required(login_url='login/')
 def create_task(request):
     if request.method == 'POST':
         db = Task()
@@ -67,3 +70,23 @@ def create_task(request):
         return redirect('todolist:todolist')
 
     return render(request, 'create-task.html')
+
+def show_json(request):
+
+    data = Task.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@login_required(login_url='login/')
+def create_ajax(request):
+    if request.method == "POST":
+        data_title = request.POST.get("title", "")
+        data_desc = request.POST.get("desc", "")
+        model = Task(
+                    user = request.user,
+                    title=data_title,
+                    description=data_desc)
+        model.save()
+        return redirect('todolist:todolist')
+
+    return render(request, 'todolist.html')
